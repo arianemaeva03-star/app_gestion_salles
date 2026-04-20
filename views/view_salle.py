c35e7c173d1768e1e9e80c8573341eb757c83e47
Maeva <arianemaeva.diffotsafack@monboreal.ca>
2026-04-20 05:30:28 -0700

    affichage des listes

--- views/view_salle.py
+++ views/view_salle.py
@@ -73,3 +73,9 @@ class ViewSalle(ctk.CTk):
             self.entry_type.insert(0, s.type)
             self.entry_capacite.delete(0, "end")
             self.entry_capacite.insert(0, s.capacite)
+
+    def lister_salles(self):
+        self.treeList.delete(*self.treeList.get_children())
+        liste = self.service_salle.recuperer_salles()
+        for s in liste:
+            self.treeList.insert("", "end", values=(s.code, s.libelle, s.type, s.capacite))
