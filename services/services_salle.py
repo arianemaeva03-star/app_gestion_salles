2d25a53b30717ba3cf5f1614e6f303b65d8f41dc
Maeva <arianemaeva.diffotsafack@monboreal.ca>
2026-04-20 04:35:29 -0700

    implementation methode modifier

--- services/services_salle.py
+++ services/services_salle.py
@@ -15,3 +15,15 @@ class ServiceSalle:
             self.dao_salle.insert_salle(salle)
             print(f"la salle {salle.code} est ajoutée")
             return True
+
+    def modifier_salle(self, salle):
+        if not salle.code or not salle.libelle or not salle.type or not salle.capacite:
+            print("données pas complete")
+            return False
+        if int(salle.capacite) < 1:
+            print("erreur capacité invalide ")
+            return False
+        else:
+            self.dao_salle.update_salle(salle)
+            print(f"la salle,{salle.code} est modifiée")
+            return True
