dd1affd76d043a12de50982557e1d28c7f84181d
Maeva <arianemaeva.diffotsafack@monboreal.ca>
2026-04-20 03:46:53 -0700

    test methode insert

--- main.py
+++ main.py
@@ -8,15 +8,22 @@ try:
 except Exception as e:
     print("pas de connexion", e)
 
-s1= Salle("C208", "Informatique" , "salle_cours", 30)
+s1= Salle("C211", "Informatique" , "salle_cours", 30)
 s2= Salle("C234", "medecine" , "laboratoire", 25)
 s3= Salle("C401", "etudes" , "librairie", 2)
 s4= Salle("C314", "aide_sociale" , "auditaurium", 80)
 
-dao.insert_salle(s1)
-dao.insert_salle(s2)
-dao.insert_salle(s3)
-dao.insert_salle(s4)
+try:
+    dao.insert_salle(s1)
+    print(f"la salle {s1.code} ajoutée")
+    dao.insert_salle(s2)
+    print(f"la salle {s2.code} ajoutée")
+    dao.insert_salle(s3)
+    print(f"la salle {s3.code} ajoutée")
+    dao.insert_salle(s4)
+    print(f"la salle {s4.code} ajoutée")
+except Exception as e:
+    print("pas d'insertion", e)
 
 
 
