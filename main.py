f48ce7005714e61da1b8baad15162d011fc1598a
Maeva <arianemaeva.diffotsafack@monboreal.ca>
2026-04-20 02:59:32 -0700

    test insertion class

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ try:
 except Exception as e:
     print("pas de connexion", e)
 
+s1= Salle(208, "Informatique" , "salle_cours", 30)
+s2= Salle(234, "medecine" , "laboratoire", 25)
+s3= Salle(401, "etudes" , "librairie", 2)
+s4= Salle(314, "aide_sociale" , "auditaurium", 80)
+try:
+   data.insert_salle(s1)
+   print(f"la salle dont le code est {s1.code}, est ajoutée")
+except Exception as e:
+   print("class pas inserée")
+
