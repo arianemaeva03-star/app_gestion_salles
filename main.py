4e70dc29db704dcb7c43d23f95551b8d90b9cdc4
Maeva <arianemaeva.diffotsafack@monboreal.ca>
2026-04-20 04:10:56 -0700

    test methode update

--- main.py
+++ main.py
@@ -30,6 +30,12 @@ print("\n")
 dao.delete_salle("C401")
 print(f" la salle C401 a ete supprimer")
 
+print("\n")
+s4_mod=Salle("C314", "aide_sociale" , "auditaurium", 80)
+dao.update_salle(s4_mod)
+print(f" la salle C314 a ete modifiée")
+
+
 
 
 
