3bd7069a0118b54c5c5807a1ce351875629a450b
Maeva <arianemaeva.diffotsafack@monboreal.ca>
2026-04-19 22:55:13 -0700

    Implementation de la methode inert_salle

--- Data/dao_salle.py
+++ Data/dao_salle.py
@@ -1,11 +1,24 @@
 import json
 import mysql.connector
+from mysql.connector import cursor
+
 from models.salle import Salle
 
 class DataSalle:
     def get_connection(self):
         with open("./Data/config.json", "r", encoding="utf-8") as f:
              config = json.load(f)
-        conn=mysql.connector.connect(host=config["host"], user=config["user"],
+        con=mysql.connector.connect(host=config["host"], user=config["user"],
                                     password=config["password"], database=config["database"])
-        return conn
+        return con
+
+    def insert_salle(self, salle):
+        con = self.get_connection()
+        crs = con.cursor()
+        crs.execute("insert into Salle values(%s,%s,%s,%s)",
+                    (salle.code, salle.libelle, salle.type, salle.capacite)
+                    )
+        con.commit()
+        crs.close()
+        con.close()
+
