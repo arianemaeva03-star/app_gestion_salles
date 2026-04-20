7008b94151cce2630995424e133fa7f694be68d9
Maeva <arianemaeva.diffotsafack@monboreal.ca>
2026-04-20 04:28:50 -0700

    implementation methode ajouter

--- services/services_salle.py
+++ services/services_salle.py
@@ -3,3 +3,15 @@ from Data.dao_salle import DataSalle
 class ServiceSalle:
     def __init__(self):
         self.dao_salle = DataSalle()
+
+    def ajouter_salle(self, salle):
+        if not salle.code or not salle.libelle or not salle.type or not salle.capacite:
+            print("toutes les donnéés doivent etres remplis")
+            return False
+        if int(salle.capacite) < 1:
+            print("Capacité doit etre minimum 1")
+            return False
+        else:
+            self.dao_salle.insert_salle(salle)
+            print(f"la salle {salle.code} est ajoutée")
+            return True
